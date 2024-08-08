f72ca9fc51f00d78171e4c8cbaddbc7d0cbc0f1c
phynnoex <bankoletheophilus21@gmail.com>
2024-08-08 00:45:32 -0400

    second-commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Player from "./components/Player"
+function App() {
+  
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name= "player 1" symbol= "X"/>
+          <Player name= "player 2" symbol= "O"/>
+        </ol>
+      </div>
+
+      LOG
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Player.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+export default function Player({name,symbol}) {
+const [isEditing, setIsEditing] = useState(false);
+function handleClick() {
+    setIsEditing(true)
+}
+
+    return(
+        <li>
+            <span className="player">
+              {(!isEditing) && <span className="player-name">{name}</span>}
+              {(isEditing) && <input></input>}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleClick}>Edit</button>
+          </li>
+    )
+}
